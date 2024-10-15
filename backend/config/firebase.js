45e36d417dbe5b44cf8517bd7bf10dd3db0bb47b
prasangeet <b23ch1033@iitj.ac.in>
2024-10-15 13:16:47 +0530

    Corrected firebase error

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,15 +1,20 @@
 import admin from 'firebase-admin';
-import { join } from 'path';
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = join(__filename, '..');
-
-const serviceAccount = JSON.parse(readFileSync(join(__dirname, 'serviceAccountKey.json'), 'utf8'));
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Ensure newlines in the private key are handled correctly
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -18,7 +23,7 @@ admin.initializeApp({
 
 const db = admin.firestore();
 const bucket = admin.storage().bucket();
-const auth = admin.auth(); 
+const auth = admin.auth();
 
 console.log('Firebase Storage Bucket:', process.env.FIREBASE_STORAGE_BUCKET);
 
